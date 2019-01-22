9d73c18a4849b64fd0ea5ff141d17d70dbff3d59
Raphael Andrade <raphael.andrade@airtecnologia.com.br>
2019-01-21 23:13:27 -0800

    configure firebase

--- src/store.js
+++ src/store.js
@@ -25,4 +25,24 @@ const rrfConfig = {
 // Init firebase instace
 firebase.initializeApp(firebaseConfig);
 //Init firestore
-const firestore = firebase.firestore();
+//const firestore = firebase.firestore();
+// Add reduxFirestore store enhancer to store creator
+const createStoreWithFirebase = compose(
+  reduxFirestore(firebase, rrfConfig), // firebase instance as first argument, rfConfig as optional second
+)(createStore)
+
+// Add Firebase to reducers
+const rootReducer = combineReducers({
+  firestore: firestoreReducer
+})
+
+//Create a initial state
+const initialState = {}
+
+//Create store
+const store = createStoreWithFirebase(rootReducer, initialState, compose(
+  reactReduxFirebase(firebase),
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+));
+
+export default store;
